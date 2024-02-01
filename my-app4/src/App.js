739c6ce7c83490dc559ea99b9399e3d86d8f206c
hhmkorea <yuyee81@gmail.com>
2024-02-01 22:35:06 +0900

    React & Ajax init

---
+++ my-app4/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WEB</h1>
+      <nav>
+        <ul>
+          <li><a href="1">HTML</a></li>
+          <li><a href="2">CSS</a></li>
+          <li><a href="3">JS</a></li>
+        </ul>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, React &amp; Ajax
+      </article>
+    </div>
+  );
+}
+
+export default App;
