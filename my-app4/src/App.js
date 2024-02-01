c9742f4a0544be9e1cb6d22ee9e145b57b7549ea
hhmkorea <yuyee81@gmail.com>
2024-02-01 23:06:38 +0900

    state에 JSON 데이타 저장, state의 list 값을 통해 목록 항목 생성

--- my-app4/src/App.js
+++ my-app4/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+class Nav extends Component {
+    state = {
+        list:[]
+    }
+
+    componentDidMount() {
+        fetch('list.json')
+            .then(function (result) {
+                return result.json();
+            })
+            .then(function (json) {
+                console.log(json);
+                this.setState({list:json});
+            }.bind(this));
+    }
+
+    render() {
+        let listTag = [];
+        for (let i = 0; i < this.state.list.length; i++) {
+            let li = this.state.list[i];
+            listTag.push(
+                <li key={li.id}>
+                    <a href={li.id}>{li.title}</a>
+                </li>
+            );
+        }
+        return (
+            <nav>
+                <ul>
+                    {listTag}
+                </ul>
+            </nav>
+        );
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <h1>WEB</h1>
-      <nav>
-        <ul>
-          <li><a href="1">HTML</a></li>
-          <li><a href="2">CSS</a></li>
-          <li><a href="3">JS</a></li>
-        </ul>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, React &amp; Ajax
-      </article>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>WEB</h1>
+            <Nav></Nav>
+            <article>
+                <h2>Welcome</h2>
+                Hello, React &amp; Ajax
+            </article>
+        </div>
+    );
 }
 
 export default App;
