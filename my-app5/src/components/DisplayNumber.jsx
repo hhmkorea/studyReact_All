e3672ed1226536dc995a6eb15c1c585f79d0133d
hhmkorea <yuyee81@gmail.com>
2024-02-03 23:05:53 +0900

    .

--- my-app5/src/components/DisplayNumber.jsx
+++ my-app5/src/components/DisplayNumber.jsx
@@ -1,19 +1,11 @@
 import React, {Component} from "react";
-import store from "../store";
 
 export default class DisplayNumber extends Component {
-    state = {number:store.getState().number}
-    constructor(props) {
-        super(props);
-        store.subscribe(function () {
-            this.setState({number:store.getState().number});
-        }.bind(this));
-    }
     render() {
         return (
             <div>
                 <h1>DisplayNumber</h1>
-                합 : <input type="text" value={this.state.number} readOnly></input>
+                합 : <input type="text" value={this.props.number} readOnly></input>
             </div>
         );
     }
