18c1336ee16ae19140e5af26147d7f7f1079ddbe
hhmkorea <yuyee81@gamil.com>
2024-02-29 20:10:17 +0900

    useState

--- my-react/my-app/src/App.js
+++ my-react/my-app/src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 import Wood from "./Wood";
+import {useState} from "react";
+import Sub from "./Sub";
 
-function App() { //A
+function App() {
+    //let number = 1; // 상태 값
 
-    let list = [1, 2, 3];
+    const [number, setNumber] = useState(2); // 2가 default값
+    // React안에 hooks 라이브러리 상태값이 됨. React 엔진이 상태값 체크!
 
+    const add = () => {
+        //number++; // 자기에게 다시 +1넣는것
+        setNumber(number + 1); // React한테 number값 변경할께 라고 요청.
+        console.log("add", number);
+    }
+
+    // 랜더링 시점 = 상태값 변경, setNumber 호출될때, Dom은 신경 안쓰고 상태값만 변경.
     return (
         <div>
-            <div>
-                {list.map((n) => (n))}
-            </div>
-            <div>
-                {list.map((n) => (
-                    <h1>{n}</h1>
-                ))}
-            </div>
+            <h1>숫자 : {number}</h1>
+            <button onClick={add}>더하기</button>
+            {/* onClick={add()}로 넣으면 바로 실행하는 것 */}
+            <Sub />
         </div>
     );
 }
