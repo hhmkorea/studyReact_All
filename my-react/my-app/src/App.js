783cfad2f7ed8c4df9038647239e4a834dd328af
hhmkorea <yuyee81@gamil.com>
2024-02-29 23:13:53 +0900

    useEffect : 의존리스트를 넣으면 해당 상태 변경되면 실행됨.

--- my-react/my-app/src/App.js
+++ my-react/my-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
 
     const [data, setData] = useState(0);
+    const [search, setSearch] = useState(0);
 
     const download = () => {
         // 다운로드 받고 (통신)
@@ -14,13 +15,17 @@ function App() {
     // 실행시점 : 
     // (1) App() 그림이 최초 그려질 때 : App() 함수가 최초 실행될 때 (= 마운트될 때),
     // (2) 상태 변수가 변경될 때
+    // (3) 의존리스트로 관리를 할 수 있다.
     useEffect(() => { // 화살표 함수 사용!!! ()=>{}, function(){}와 동일!!
         console.log("useEffect 실행됨");
         download();
-    }, []);
+    }, [search]);
     
     return (
         <div>
+            <button onClick={() => {
+                setSearch(2);
+            }}>검색하기</button>
             <h1>데이터 : {data}</h1>
             <button onClick={()=>{
                 setData(data+1)
