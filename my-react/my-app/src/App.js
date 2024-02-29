e8022ef1fde359171d13766cbb59b55c217d55b6
hhmkorea <yuyee81@gamil.com>
2024-02-29 21:26:33 +0900

    useState : 불변 데이터에 데이터에 일부데이터 변경하기.

--- my-react/my-app/src/App.js
+++ my-react/my-app/src/App.js
@@ -7,6 +7,8 @@ function App() {
 
     console.log("App 실행됨");
 
+    const [ num, setNum] = useState(5);
+
     let sample = [ // 초기값 세팅을 제일 위로 해둬야 referece 변경 안됨.
         {id: 1, name: "홍길동"},
         {id: 2, name: "임꺽정"},
@@ -20,18 +22,8 @@ function App() {
     const download = () => {
         //fetch().then().then()
         // 기존 데이터 세팅
-
-        //setUsers(sample);                     // reference 변경 없음.
-        //setUsers([...sample]);                // 깊은 복사로, reference 변경. 강제로 다시 랜더링됨.
-
-        //sample.push({id:5, name: "조자룡"});   // 얇은 복사로, refernce 변경 없이 값만 변경됨. useState() 랜더링 안됨.
-        //console.log(sample);
-        //setUsers(sample);
-
-        const a = sample.concat({id:5, name: "조자룡"}); // 계속 랜더링됨. but 기존 데이타 sample불변, 똑같은 데이터라 한번만 추가됨.
-        console.log(a);
-        setUsers(a);
-
+        setUsers([...sample, {id:num, name: "조자룡"}]);
+        setNum(num + 1);
     };
 
     return (
