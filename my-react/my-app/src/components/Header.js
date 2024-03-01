ebd4bd0ea315938699aaea8aae57242c07fa7615
hhmkorea <yuyee81@gamil.com>
2024-03-01 20:34:37 +0900

    components styled : 변수명 명명규칙으로 변경

--- my-react/my-app/src/components/Header.js
+++ my-react/my-app/src/components/Header.js
@@ -4,19 +4,19 @@ import styled from "styled-components";
 // 하나의 컴포넌트를 생성(재사용)
 
 // styled-components => js파일과 css파일 관리!!!
-const HeaderList = styled.div`
+const StyledHeaderBtnDiv = styled.div`
     border: 1px solid black;
     height: 300px;
 `;
 
 const Header = () => {
     return (
-        <HeaderList>
+        <StyledHeaderBtnDiv>
             <ul>
                 <li>메뉴1</li>
                 <li>메뉴2</li>
             </ul>
-        </HeaderList>
+        </StyledHeaderBtnDiv>
     );
 };
 
