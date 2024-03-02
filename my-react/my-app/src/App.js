ac45447a8348f09bebdfc4035eb27f9ed92c6a2f
hhmkorea <yuyee81@gamil.com>
2024-03-02 21:41:28 +0900

    게시판 만들기 : 데이터가 DB가 아닌 소스 안에서 하드코딩한거, Route빼고 글쓰기 기능은 목록에 같이 넣음.

--- my-react/my-app/src/App.js
+++ my-react/my-app/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
 import ListPage from "./pages/ListPage";
-import WritePage from "./pages/WritePage";
-import Navigation from "./components/Navigation";
 
 // 글쓰기, 글삭제, 글목록보기
-function App(props) {
+function App() {
+
     return (
         <div>
-            <Navigation />
-            <Routes>
-                <Route path="/" exact={true} element={<ListPage />} />
-                <Route path="/write" exact={true} element={<WritePage />} />
-            </Routes>
+            <ListPage />
+
         </div>
     );
 }
--- my-react/my-app/src/pages/ListPage.js
+++ my-react/my-app/src/pages/ListPage.js
@@ -10,6 +10,12 @@ const StyledItemBoxDiv = styled.div`
     margin: 20px;
     align-items: center; // 아이템 왼쪽 중앙 정렬
 `;
+
+const handleWrite = () => {
+    // ListPage의 setPosts에 무엇을 담아야함?
+    let post = {id:6, title:"인풋값"};
+}
+
 const ListPage = () => {
 
     const [posts, setPost] = useState([
@@ -22,14 +28,19 @@ const ListPage = () => {
     return (
         <div>
             <h1>리스트 페이지</h1>
+            <form>
+                <input type="text" placeholder="제목을 입력하세요..."/>
+                <button type="button" onClick={handleWrite}>글쓰기</button>
+                {/* type="button"은 submit이 안됨.*/}
+            </form>
             <hr/>
             {posts.map((post) => (
                 <StyledItemBoxDiv key={post.id}>
                     <div>
-                    번호 : {post.id} 제목 : {post.title}
+                        번호 : {post.id} 제목 : {post.title}
                     </div>
                     <div>
-                    <button>삭제</button>
+                        <button>삭제</button>
                     </div>
                 </StyledItemBoxDiv>
             ))}
