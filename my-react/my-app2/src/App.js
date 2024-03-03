8d77f079239854daddaf46e74834a8a07061c3dd
hhmkorea <yuyee81@gamil.com>
2024-03-03 19:47:05 +0900

    .

--- my-react/my-app2/src/App.js
+++ my-react/my-app2/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import ListPage from "./pages/ListPage";
 
-// 글쓰기, 글삭제, 글목록보기
-function App() {
-
-    return (
-        <div>
-            <ListPage /> {/* 상태값을 들고 있음, 글쓰기 기능도 넣음 */}
-        </div>
-    );
+function App(props) {
+  return (
+    <div></div>
+  );
 }
 
 export default App;
